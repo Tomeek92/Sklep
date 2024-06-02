6596b0ca26251b4323113e4a441047f40c2f7605
Tomeek92 <98541483+Tomeek92@users.noreply.github.com>
2024-06-02 15:36:32 +0200

    CRUD2

---
+++ Sklep/wwwroot/js/check.js
@@ -0,0 +1,9 @@
+﻿
+    function toggleCheck(description) {
+        var productElement = document.getElementById('product-' + description);
+    if (productElement.style.textDecorationLine === 'line-through') {
+        productElement.style.textDecorationLine = 'none';
+        } else {
+        productElement.style.textDecorationLine = 'line-through';
+        }
+    }
